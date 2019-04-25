9ff9bd032355d0094157422ebbdb9c5b4db0bdab
Luís Sousa (canuto491) <canuto491@gmail.com>
2019-04-25 11:14:22 +0100

    refactor

--- index.js
+++ index.js
@@ -1,45 +1,53 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
+    let municipalities = await getMadeiraMunicipalities();
+    console.log('municipalities', municipalities);
+})();
+
+
+async function getMadeiraMunicipalities() {
     const ulr = 'https://pt.wikipedia.org/wiki/Lista_de_freguesias_da_Madeira';
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(ulr);
     // await page.screenshot({path: 'example.png'});
-    const html = await page.content();
+    // const html = await page.content();
 
-    let municipios = await page.evaluate(
-        () => {
-            let municipios = [ // municipios
-                ...document.querySelectorAll('.thumbinner .thumbcaption .legend')
-            ].map((nodeMunicipalitie) => (
-                {
-                    municipio: nodeMunicipalitie.innerText.trim(),
-                    freguesias: [],
-                }
-            ))
+    let municipios = await page.evaluate(processMunicipalities);
 
-            const freguesiasList = [ // freguesias
-                ...document.querySelectorAll('table.prettytable tbody tr:not(:first-child)')
-            ].map((nodeParish) => (
-                {
-                    freguesia: nodeParish.querySelector('td:nth-child(1)').innerText.trim(),
-                    municipio: nodeParish.querySelector('td:nth-child(2)').innerText.trim(),
-                }
-            ))
-
-            municipios.forEach(function(municipio) {
-                freguesiasList.forEach(function(freg) {
-                    if (freg.municipio.localeCompare(municipio.municipio) === 0) {
-                        municipio.freguesias.push(freg.freguesia)
-                    }
-                })
-            });
-
-            return municipios;
-        }
-    );
-    console.log(municipios);
     await browser.close();
-})();
+    return municipios;
+}
+
+
+function processMunicipalities() {
+    let municipalities = [ // municipios
+        ...document.querySelectorAll('.thumbinner .thumbcaption .legend')
+    ].map((nodeMunicipalitie) => (
+        {
+            municipalitieName: nodeMunicipalitie.innerText.trim(),
+            parishes: [],
+        }
+    ))
+
+    const parishes = [ // freguesias
+        ...document.querySelectorAll('table.prettytable tbody tr:not(:first-child)')
+    ].map((nodeParish) => (
+        {
+            parishes: nodeParish.querySelector('td:nth-child(1)').innerText.trim(),
+            municipalitie: nodeParish.querySelector('td:nth-child(2)').innerText.trim(),
+        }
+    ))
+
+    municipalities.forEach(function(municipalitie) {
+        parishes.forEach(function(parish) {
+            if (parish.municipalitie.localeCompare(municipalitie.municipalitieName) === 0) {
+                municipalitie.parishes.push(parish.parishes)
+            }
+        })
+    });
+
+    return municipalities;
+}
